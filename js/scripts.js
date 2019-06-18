d032d36fb62ac2b4f568735760d3835dba2f6ad7
Dominic Montelongo <montelongodominic@email.com>
2019-06-17 17:12:34 -0700

    Unable to locate bug in js and about.html 'submit' which routes user to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $('form#coding').submit(function (event) {
-    event.preventDefault();
     var office = parseInt($('#office').val());
     var beverage = parseInt($('#beverage').val());
     var pet = parseInt($('#pet').val());
@@ -8,18 +7,14 @@ $(document).ready(function () {
     var pampering = parseInt($('#pampering').val());
     var transport = parseInt($('#transport').val());
     var feet = parseInt($('#feet').val());
-    console.log(office);
-    console.log(beverage);
 
     var score = office + beverage + pet + wakeUp + pampering + transport + feet;
 
-    console.log(score);
 
     if (score < 8) {
       $("#program1").show();
       $("#program1").hide();
       $("#program1").hide();
-      // alert('Consider Ruby, Python, and Javascript in all locations');
     } else if (score > 7 && 15 > score) {
       $("#program2").show();
       $("#program1").hide();
@@ -28,12 +23,13 @@ $(document).ready(function () {
       $("#program3").show());
       $("#program1").hide();
       $("#program1").hide();
-    } else if (score > 22 && 29 > score) {
+    } else  (score > 22 && 29 > score) {
       $("#program1").show());
       $("#program2").show();
       $("#program3").show();
     };
   });
+  event.preventDefault();
 });
 
 
